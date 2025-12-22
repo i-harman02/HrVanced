daaa10baec9dca565926d3bce02d70bd9b270388
RajeevkumarRathore <rajeevkumarrathore@yahoo.co.in>
2025-12-22 16:29:41 +0530

    new commit

--- src/pages/Mail.jsx
+++ src/pages/Mail.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Mail = () => {
   return (
     <div>
-      mail
+        <div className="bg-white p-8 border border-bordergray rounded-xl">
+          <div className="mb-6">
+            <h1 className="text-2xl font-bold text-heading leading-tight">
+              Inbox
+            </h1>
+          </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mail
+export default Mail;

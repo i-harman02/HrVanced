e709061c3cdb4dfa75ed8a791786f9b77e83b475
RajeevkumarRathore <rajeevkumarrathore@yahoo.co.in>
2026-02-03 11:04:58 +0530

    Updated Policy Modal

--- src/components/AttendancePolicyModal.jsx
+++ src/components/AttendancePolicyModal.jsx
@@ -4,51 +4,47 @@ import { RxCross2 } from "react-icons/rx";
 const AttendancePolicyModal = ({ onClose }) => {
   return (
     <>
-      {/* Overlay */}
       <div
-        className="fixed inset-0 bg-black/40 z-50"
+        className="fixed inset-0 bg-black/60 z-50"
         onClick={onClose}
       />
 
-      {/* Modal */}
       <div className="fixed inset-0 z-60 flex items-center justify-center px-4">
-        <div className="bg-white w-full max-w-2xl rounded-lg shadow-xl">
+        <div className="bg-white w-full max-w-173.5 rounded-lg shadow-xl">
           {/* Header */}
-          <div className="flex items-center justify-between px-6 py-4 border-b">
-            <h2 className="text-lg font-semibold text-gray-900">
+          <div className="flex items-center justify-between p-6 md:p-8 border-b border-bordergray">
+            <h2 className="text-2xl font-bold text-black leading-tight">
               Attendance Policy
             </h2>
 
             <button
               onClick={onClose}
-              className="text-gray-500 hover:text-black"
+              className="text-black cursor-pointer"
             >
               <RxCross2 size={22} />
             </button>
           </div>
 
-          {/* Content */}
-          <div className="px-6 py-5 text-sm text-gray-700 space-y-6">
-            {/* Attendance Policy */}
+          <div className="p-6 md:px-8 md:pt-6 md:pb-8 text-sm text-textgray space-y-6">
             <div>
-              <h3 className="font-semibold mb-2">
+              <h3 className="font-semibold text-black text-base mb-3">
                 Attendance Policy:
               </h3>
               <ul className="list-disc pl-5 space-y-1">
                 <li>
-                  <span className="font-medium">Start Time:</span>{" "}
+                  <span className="font-medium text-black">Start Time:</span>{" "}
                   All employees must report to work by 9:30 a.m. sharp.
                 </li>
                 <li>
-                  <span className="font-medium">Lateness/Absence:</span>{" "}
+                  <span className="font-medium text-black">Lateness/Absence:</span>{" "}
                   Inform HR and TL immediately in case of delay or absence.
                 </li>
                 <li>
-                  <span className="font-medium">Leave Approval:</span>{" "}
+                  <span className="font-medium text-black">Leave Approval:</span>{" "}
                   Submit planned leave requests via Portal to your TL and HR in advance.
                 </li>
                 <li>
-                  <span className="font-medium">Documentation:</span>{" "}
+                  <span className="font-medium text-black">Documentation:</span>{" "}
                   Provide valid documents for any lateness, absences, or early departures.
                 </li>
               </ul>
@@ -56,18 +52,18 @@ const AttendancePolicyModal = ({ onClose }) => {
 
             {/* Hours of Work */}
             <div>
-              <h3 className="font-semibold mb-2">
+              <h3 className="font-semibold text-black text-base mb-3">
                 Hours of Work: (Workweek)
               </h3>
               <ul className="list-disc pl-5 space-y-1">
                 <li>
-                  <span className="font-medium">
+                  <span className="font-medium text-black">
                     Monday to Friday (Trainees/Interns:
                   </span>{" "}
                   Monday to Saturday).
                 </li>
                 <li>
-                  <span className="font-medium">Working hours:</span>{" "}
+                  <span className="font-medium text-black">Working hours:</span>{" "}
                   9:30 a.m. to 7:00 p.m. (Rotations as per project requirement)
                 </li>
               </ul>
@@ -75,16 +71,16 @@ const AttendancePolicyModal = ({ onClose }) => {
 
             {/* Breaks */}
             <div>
-              <h3 className="font-semibold mb-2">
+              <h3 className="font-semibold text-black text-base mb-3">
                 Breaks:
               </h3>
               <ul className="list-disc pl-5 space-y-1">
                 <li>
-                  <span className="font-medium">Lunch:</span>{" "}
+                  <span className="font-medium text-black">Lunch:</span>{" "}
                   30 Minutes during 1:30 p.m. to 2:30 p.m.
                 </li>
                 <li>
-                  <span className="font-medium">Tea:</span>{" "}
+                  <span className="font-medium text-black">Tea:</span>{" "}
                   15–15 Minutes during 10:30 a.m.–11:00 a.m. and 4:00 p.m.–4:30 p.m.
                 </li>
               </ul>
